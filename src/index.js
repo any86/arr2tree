a2dd4b1930e6b021c21405149491546891829db1
宁宁 <383514580@qq.com>
2019-11-12 10:37:16 +0800

    feat: 增加assessRoot用来自定义判断根节点函数

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 /**
  * 数组转树
  * @param {Array} 输入
- * @param {Object.String} keyMap.KEY_ID 表示唯一性键值(id)
- * @param {Object.String} keyMap.KEY_PID 对应的父id
- * @param {Object.String} keyMap.KEY_ORDER 排序依据的键值
+ * @param {Object.String} Options.KEY_ID 表示唯一性键值(id)
+ * @param {Object.String} Options.KEY_PID 对应的父id
+ * @param {Object.String} Options.KEY_ORDER 排序依据的键值
+ * @param {Object.Function} Options.assessRoot 判断是否根节点, 接收一个参数(当前循环的节点)
  * @param {Function} 每次循环执行, 返回"节点"和"是否根节点"
  * @returns {Object} 树结构
  */
-module.exports = function(array, keyMap = {
-    KEY_ID: 'id',
-    KEY_PID: 'pid',
-    KEY_ORDER: 'order'
-}, callback = () => {}) {
-    const {
-        KEY_ID,
-        KEY_PID,
-        KEY_ORDER
-    } = keyMap;
+module.exports = function(array,options={}, callback = () => {}) {
+    // 默认值
+    const KEY_ID = options.KEY_ID || 'id';
+    const KEY_PID = options.KEY_PID || 'pid';
+    const KEY_ORDER = options.KEY_ORDER || 'order';
+    const assessRoot = 'function' === typeof options.assessRoot ? options.assessRoot : node => !!node[KEY_PID];
 
     const tree = [];
     let nodeMap = {};
@@ -25,7 +22,7 @@ module.exports = function(array, keyMap = {
         const id = node[KEY_ID];
         const pid = node[KEY_PID];
 
-        if (pid) {
+        if (assessRoot(node)) {
             // 非根节点
             nodeMap[pid] = nodeMap[pid] || [];
             nodeMap[pid].push(node);
