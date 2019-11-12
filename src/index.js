8881ba4f4e49ed4310059096bb17a032d7c081ae
宁宁 <383514580@qq.com>
2019-11-12 11:46:09 +0800

    refactor: 修改默认参数逻辑

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 /**
  * 数组转树
  * @param {Array} 输入
- * @param {Object.String} Options.KEY_ID 表示唯一性键值(id)
- * @param {Object.String} Options.KEY_PID 对应的父id
- * @param {Object.String} Options.KEY_ORDER 排序依据的键值
- * @param {Object.Function} Options.assessRoot 判断是否根节点, 接收一个参数(当前循环的节点)
+ * @param {Object.String} options.KEY_ID 表示唯一性键值(id)
+ * @param {Object.String} options.KEY_PID 对应的父id
+ * @param {Object.String} options.KEY_ORDER 排序依据的键值
+ * @param {Object.Function} options.assessRoot 判断是否根节点, 接收一个参数(当前循环的节点)
  * @param {Function} 每次循环执行, 返回"节点"和"是否根节点"
  * @returns {Object} 树结构
  */
-module.exports = function(array,options={}, callback = () => {}) {
+module.exports = function(array, {
+    KEY_ID = 'id',
+    KEY_PID = 'pid',
+    KEY_ORDER = 'order',
+    assessRoot =  void 0
+} = {}, callback = () => {}) {
     // 默认值
-    const KEY_ID = options.KEY_ID || 'id';
-    const KEY_PID = options.KEY_PID || 'pid';
-    const KEY_ORDER = options.KEY_ORDER || 'order';
-    const assessRoot = 'function' === typeof options.assessRoot ? options.assessRoot : node => !!node[KEY_PID];
+    const _assessRoot = 'function' === typeof assessRoot ? assessRoot : node => !!node[KEY_PID];
 
     const tree = [];
     let nodeMap = {};
@@ -22,7 +24,7 @@ module.exports = function(array,options={}, callback = () => {}) {
         const id = node[KEY_ID];
         const pid = node[KEY_PID];
 
-        if (assessRoot(node)) {
+        if (_assessRoot(node)) {
             // 非根节点
             nodeMap[pid] = nodeMap[pid] || [];
             nodeMap[pid].push(node);
