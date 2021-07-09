247ba9c443b4f70b34939401429e039290831964
Russell <383514580@qq.com>
2021-07-09 10:50:27 +0800

    chore: 忽略非必须文件

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,9 @@
 declare type Node = Record<string | number, any>;
-declare const _default: (array: any[], { KEY_ID, KEY_PID, KEY_ORDER, assessRoot, transform }?: {
+declare const _default: (array: Node[], { KEY_ID, KEY_PID, KEY_ORDER, assessRoot, transform }?: {
     KEY_ID?: string | undefined;
     KEY_PID?: string | undefined;
     KEY_ORDER?: string | undefined;
     assessRoot?: undefined;
-    transform?: ((node: Node, isRoot: boolean) => any) | undefined;
-}) => any[];
+    transform?: ((node: Node, isRoot: boolean) => Node) | undefined;
+}) => Node[];
 export = _default;
