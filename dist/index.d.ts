aff364f48e6e0ba08299bc7774be0d927b5417b0
Russell <383514580@qq.com>
2021-07-09 11:13:08 +0800

    chore(release): 0.0.6

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Node = Record<string | number, any>;
-declare const _default: (array: Node[], { KEY_ID, KEY_PID, KEY_ORDER, assessRoot, transform }?: {
-    KEY_ID?: string | undefined;
-    KEY_PID?: string | undefined;
-    KEY_ORDER?: string | undefined;
-    assessRoot?: undefined;
-    transform?: ((node: Node, isRoot: boolean) => Node) | undefined;
-}) => Node[];
-export = _default;
