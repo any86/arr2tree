ea6c6532458870b6834035b35d4e0af2cd58f983
Russell <383514580@qq.com>
2022-03-29 17:15:43 +0800

    refactor: 取消KEY_ORDER字段, 使用compareOrder函数替代

--- index.ts
+++ index.ts
@@ -1,31 +1,30 @@
 import quickSort from '@any86/quick-sort'
 type Node = Record<string | number, any>;
+type NodeMap = Record<string, Node[]>;
+type CompareOrder = (a: Node, b: Node) => number;
+
 /**
  * 数组转树
  * @param 输入数组
  * @param options.KEY_ID 表示唯一性键值(id)
  * @param options.KEY_PID 对应的父id
- * @param options.KEY_ORDER 排序依据的键值
+ * @param options.compareOrder 排序函数, 默认根据order的值倒序
  * @param options.isRoot 判断是否根节点, 接收一个参数(当前循环的节点)
  * @param options.transform 控制节点的返回格式
  * @returns 树结构
  */
-//  (node:Node)=>boolean
 const DEFAULT_OPTIONS = {
     KEY_ID: 'id',
     KEY_PID: 'pid',
-    KEY_ORDER: 'order',
     KEY_CHILDREN: 'children',
+    compareOrder: ((a: Node, b: Node) => b.order - a.order) as CompareOrder,
     transform: (node: Node): Node | void => node,
     isRoot: (node: Node) => !node[DEFAULT_OPTIONS.KEY_PID]
 }
 
-
-
-type NodeMap = Record<string, Node[]>;
 export default function (array: Node[], options?: Partial<typeof DEFAULT_OPTIONS>) {
     // 默认值
-    const { KEY_ID, KEY_ORDER, KEY_CHILDREN, KEY_PID, transform, isRoot } = { ...DEFAULT_OPTIONS, ...options };
+    const { KEY_ID, KEY_CHILDREN, KEY_PID, compareOrder, transform, isRoot } = { ...DEFAULT_OPTIONS, ...options };
 
     let tree = [];
     let pidAndChildrenMap: NodeMap | null = {};
@@ -68,15 +67,8 @@ export default function (array: Node[], options?: Partial<typeof DEFAULT_OPTIONS
     }
 
     // 排序
-    for (const pid in pidAndChildrenMap) {
-        // pidAndChildrenMap[pid].sort((prev, current) => prev[KEY_ORDER] - current[KEY_ORDER]);
-        pidAndChildrenMap[pid] = quickSort(pidAndChildrenMap[pid], (prev, current) => prev[KEY_ORDER] - current[KEY_ORDER])
-
-    }
-    // tree.sort((prev, current) => prev[KEY_ORDER] - current[KEY_ORDER]);
-    tree = quickSort(tree, (prev, current) => prev[KEY_ORDER] - current[KEY_ORDER]);
-
-
+    for (const pid in pidAndChildrenMap) pidAndChildrenMap[pid] = quickSort(pidAndChildrenMap[pid], compareOrder)
+    tree = quickSort(tree, compareOrder);
 
     // 有循环引用, 手动销毁
     pidAndChildrenMap = null;
--- test2.js
+++ test2.js
@@ -4,33 +4,38 @@ const array = [
         "name": "首页",
         "path": "/",
         "id": "1",
-        "icon": "icon-round_rank_fill"
+        "icon": "icon-round_rank_fill",
+        "order":1
     },
     {
         "name": "系统",
         "id": "2",
         "icon": "icon-round_light_fill",
+        "order":2
     },
     {
         "name": "人员管理",
         "path": "/user",
         "id": "3",
         "pid": "2",
-        "icon": "icon-round_friend_fill"
+        "icon": "icon-round_friend_fill",
+        "order":12
     },
     {
         "name": "角色管理",
         "path": "/role",
         "id": "7",
         "pid": "2",
-        "icon": "icon-ticket_fill"
+        "icon": "icon-ticket_fill",
+        "order":1
     },
     {
         "name": "菜单管理",
         "path": "/menu",
         "id": "5",
         "pid": "2",
-        "icon": "icon-ticket_fill"
+        "icon": "icon-ticket_fill",
+        "order":999
     }
 ]
 
